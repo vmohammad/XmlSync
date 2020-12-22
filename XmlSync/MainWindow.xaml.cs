c746730a986547d3c8150c3b30704728e0975ed4
mohammadvm <mohammadvm1378@gmail.com>
2020-12-22 17:46:28 +0330

    update

--- XmlSync/MainWindow.xaml.cs
+++ XmlSync/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -62,6 +63,7 @@ namespace XmlSync
         List<XmlLanguageViewModel> xlvm = new List<XmlLanguageViewModel>();
         List<XmlLanguageModel> xlms = new List<XmlLanguageModel>();
         List<Languages> lans = new List<Languages>();
+        List<Languages> fixed_lans = new List<Languages>();
         string fileNameMain = "";
         string fileNameSec = "";
         private void ButtonReadFile_Click(object sender, RoutedEventArgs e)
@@ -199,17 +201,37 @@ namespace XmlSync
             //}
         }
 
-        public string Name { get; set; }
-        public string Value { get; set; }
-        public string[] GetElementStrings(IEnumerable<string> Input)
+
+        public static List<Languages> Load(List<Languages> allLanguages)
         {
-            int i = 0;
-            foreach (var Lines in Input)
+            for (int i = 0; i < allLanguages.Count; i++)
             {
-
+                allLanguages[i].SortModel();
             }
-
-            return null;
+            for (int i = 0; i < allLanguages.Count - 1; i++)
+            {
+                var first = allLanguages[i].LanguageModel;
+                var sec = allLanguages[i + 1].LanguageModel;
+                for (int j = 0; j < first.Count; j++)
+                {
+                    if (!sec.Any(e => e.Name == first[j].Name) && i+1>=allLanguages.Count)
+                    {
+                        var temp = first[j];
+                        temp.ID = sec.Count;
+                        allLanguages[i + 1].LanguageModel.Insert(j + 1, temp);
+                    }
+                }
+                for (int j = 0; j < sec.Count; j++)
+                {
+                    if (!first.Any(e => e.Name == sec[j].Name))
+                    {
+                        var temp = sec[j];
+                        temp.ID = first.Count;
+                        allLanguages[i].LanguageModel.Insert(j + 1, temp);
+                    }
+                }
+            }
+            return allLanguages;
         }
 
         private void ButtonSaveFile_Click(object sender, RoutedEventArgs e)
@@ -314,7 +336,7 @@ namespace XmlSync
             TextBox txtLabel = (TextBox)FindName("txtLabel" + count);
             TextBox txtPath = (TextBox)FindName("txtPath" + count);
             Button btnRemove = (Button)FindName("txtRemove" + count);
-            if (!txtLabel.Text.Equals(""))
+            if (txtLabel != null && !txtLabel.Text.Equals(""))
             {
                 OpenFileDialog openFileDialog = new OpenFileDialog();
                 openFileDialog.Filter = "XML Files|*.xml";
@@ -331,7 +353,7 @@ namespace XmlSync
                     XmlDocument xmlDoc = new XmlDocument();
                     xmlDoc.Load(temp.filePath);
                     var culture = xmlDoc.DocumentElement.SelectNodes("/localizationDictionary")[0].Attributes.GetNamedItem("culture").InnerText;
-                    txtPath.Text = culture.ToString();
+                    txtPath.Text = temp.filePath;
                     XmlNodeList nodeList = xmlDoc.DocumentElement.SelectNodes("/localizationDictionary/texts/text");
                     int i = 0;
                     foreach (XmlNode node in nodeList)
@@ -345,6 +367,60 @@ namespace XmlSync
                     lans.Add(temp);
                     btnAdd.Visibility = Visibility.Hidden;
                     txtPath.Visibility = Visibility.Visible;
+                    RowDefinition rowDef = new RowDefinition();
+                    rowDef.Name = "row" + lans.Count;
+                    rowDef.Height = GridLength.Auto;
+                    GridTop.RowDefinitions.Add(rowDef);
+                    count++;
+                    Button btnAddnext = new Button();
+                    TextBox txtLabelnext = new TextBox();
+                    TextBox txtPathnext = new TextBox();
+                    TextBox txtCountnext = new TextBox();
+                    Button btnRemovenext = new Button();
+                    //VisualState txtLabelnext = new VisualState();
+                    //txtLabelnext.SetValue(FrameworkElement..);
+                    //btnAddnext = btnAdd;
+                    //txtLabelnext = txtLabel;
+                    //txtPathnext = txtPath;
+                    //btnRemovenext = btnRemove;
+                    btnAddnext.Name = "btnAdd" + count;
+                    txtLabelnext.Name = "txtLabel" + count;
+                    txtPathnext.Name = "txtPath" + count;
+                    txtCountnext.Name = "txtCount" + count;
+                    this.RegisterName("btnAdd" + count, btnAddnext);
+                    this.RegisterName("txtLabel" + count, txtLabelnext);
+                    this.RegisterName("txtPath" + count, txtPathnext);
+                    this.RegisterName("txtCount" + count, txtCountnext);
+                    btnAddnext.SetValue(FrameworkElement.NameProperty, "btnAdd" + count);
+                    txtLabelnext.SetValue(FrameworkElement.NameProperty, "txtLabel" + count);
+                    txtPathnext.SetValue(FrameworkElement.NameProperty, "txtPath" + count);
+                    txtCountnext.SetValue(FrameworkElement.NameProperty, "txtCount" + count);
+
+                    txtCountnext.Text = count.ToString();
+                    txtPathnext.Visibility = Visibility.Hidden;
+                    btnAddnext.Content = "Add Another Language";
+                    btnAddnext.AddHandler(Button.ClickEvent, new RoutedEventHandler(btnAdd_Click));
+
+                    RowDefinition rowDef1 = new RowDefinition();
+                    GridTop.RowDefinitions.Add(rowDef1);
+
+                    Grid.SetRow(txtCountnext, count);
+                    Grid.SetRow(btnAddnext, count);
+                    Grid.SetRow(txtLabelnext, count);
+                    Grid.SetRow(txtPathnext, count);
+                    Grid.SetColumn(txtCountnext, 0);
+                    Grid.SetColumn(btnAddnext, 2);
+                    Grid.SetColumn(txtLabelnext, 1);
+                    Grid.SetColumn(txtPathnext, 2);
+                    this.GridTop.Children.Add(txtCountnext);
+                    this.GridTop.Children.Add(btnAddnext);
+                    this.GridTop.Children.Add(txtLabelnext);
+                    this.GridTop.Children.Add(txtPathnext);
+                    //btnRemovenext.SetValue(Grid.RowProperty, count);
+                    //GridTop.Children.Add(btnAddnext);
+                    //GridTop.Children.Add(txtLabelnext);
+                    //GridTop.Children.Add(txtPathnext);
+                    //GridTop.Children.Add(btnRemovenext);
                     //btnRemove.Visibility = Visibility.Visible;
                 }
             }
@@ -354,9 +430,187 @@ namespace XmlSync
             }
         }
 
+        public static void UpdateGrid()
+        {
+            DataGrid dg = new DataGrid();
+
+
+
+            DataGridTextColumn textColumn = new DataGridTextColumn();
+            textColumn.Header = "First Name";
+            textColumn.Binding = new Binding("FirstName");
+            //dgLans.Columns.Add(textColumn);
+        }
+
+        public class Field
+        {
+            public Field(string name, Type type)
+            {
+                this.FieldName = name;
+                this.FieldType = type;
+            }
+
+            public string FieldName;
+
+            public Type FieldType;
+        }
+
+        public class DynamicClass : DynamicObject
+        {
+            private Dictionary<string, KeyValuePair<Type, object>> _fields;
+
+            public DynamicClass(List<Field> fields)
+            {
+                _fields = new Dictionary<string, KeyValuePair<Type, object>>();
+                fields.ForEach(x => _fields.Add(x.FieldName,
+                    new KeyValuePair<Type, object>(x.FieldType, null)));
+            }
+
+            public override bool TrySetMember(SetMemberBinder binder, object value)
+            {
+                if (_fields.ContainsKey(binder.Name))
+                {
+                    var type = _fields[binder.Name].Key;
+                    if (value.GetType() == type)
+                    {
+                        _fields[binder.Name] = new KeyValuePair<Type, object>(type, value);
+                        return true;
+                    }
+                    else throw new Exception("Value " + value + " is not of type " + type.Name);
+                }
+                return false;
+            }
+
+            public override bool TryGetMember(GetMemberBinder binder, out object result)
+            {
+                result = _fields[binder.Name].Value;
+                return true;
+            }
+        }
+        public static void AddProperty(ExpandoObject expando, string propertyName, object propertyValue)
+        {
+            // ExpandoObject supports IDictionary so we can extend it like this
+            var expandoDict = expando as IDictionary<string, object>;
+            if (expandoDict.ContainsKey(propertyName))
+                expandoDict[propertyName] = propertyValue;
+            else
+                expandoDict.Add(propertyName, propertyValue);
+        }
+        List<XmlMultiLanguageViewModel> xmlvm = new List<XmlMultiLanguageViewModel>();
+        private List<dynamic> dataList;
         private void btnLoadFile_Click(object sender, RoutedEventArgs e)
         {
+            DataGrid dg = new DataGrid();
+            dg.AutoGenerateColumns = false;
+            dg.CanUserAddRows = false;
+            dg.CanUserDeleteRows = false;
+            dg.CanUserReorderColumns = false;
+            dg.CanUserResizeColumns = true;
+            dg.CanUserResizeRows = true;
+            dg.CanUserSortColumns = false;
+            dg.HorizontalContentAlignment = HorizontalAlignment.Center;
+            Grid.SetRow(dg, 1);
+            Grid.SetColumn(dg, 0);
+            Grid.SetColumnSpan(dg, 2);
+            this.GridMain.Children.Add(dg);
+            fixed_lans = Load(lans);
 
+            string[] lansName = new string[fixed_lans.Count];
+            for (int i = 0; i < fixed_lans.Count; i++)
+            {
+                lansName[i] = fixed_lans[i].lanLabel;
+            }
+
+            //List<string> lansName = new List<string>();
+            //for (int i = 0; i < fixed_lans.Count; i++)
+            //{
+            //    lansName.Add(fixed_lans[i].lanLabel);
+            //}
+            var fields = new List<Field>();
+            fields.Add(new Field("Name", typeof(string)));
+            for (int i = 0; i < fixed_lans.Count; i++)
+            {
+                fields.Add(new Field("ID" + fixed_lans[i].lanLabel, typeof(int)));
+                fields.Add(new Field(""+fixed_lans[i].lanLabel, typeof(string)));
+            }
+
+            dynamic obj = new DynamicClass(fields);
+
+            List<dynamic> expando = new List<dynamic>();
+
+            for (int i = 0; i < fixed_lans.Count; i++)
+            {
+                for (int j = 0; j < fixed_lans[i].LanguageModel.Count; j++)
+                {
+                    ExpandoObject eo = new ExpandoObject();
+                    if (i == 0)
+                    {
+                        AddProperty(eo, "Name", fixed_lans[i].LanguageModel[j].Name);
+                    }
+                    AddProperty(eo, "ID" + fixed_lans[i].lanLabel, fixed_lans[i].LanguageModel[j].ID);
+                    AddProperty(eo, "" + fixed_lans[i].lanLabel, fixed_lans[i].LanguageModel[j].Name);
+                    if (i+1<fixed_lans.Count)
+                    {
+                        AddProperty(eo, "ID" + fixed_lans[i + 1].lanLabel, fixed_lans[i].LanguageModel[j].ID);
+                        AddProperty(eo, "" + fixed_lans[i + 1].lanLabel, fixed_lans[i].LanguageModel[j].Name);
+                    }
+                    expando.Add(eo);
+                }
+            }
+            //dataList = new List<dynamic>()
+            //{
+            //    "en",
+            //    "fa"
+            //};
+
+            //dynamic EN = new ExpandoObject();
+            //EN.Text = fixed_lans[0].LanguageModel;
+            //dynamic FA = new ExpandoObject();
+            //FA.Text = fixed_lans[1].LanguageModel;
+            //
+            //dynamic thelist = new ExpandoObject();
+            //thelist.enText = fixed_lans[0].LanguageModel;
+            //thelist.faText = fixed_lans[1].LanguageModel;
+            //
+            //var dd = new List<dynamic>();
+            //
+            //
+            //dd.Add(thelist);
+            //dataList.Add(FA);
+
+            //dataList.Add(new { V = lansName[0],fixed_lans[0].LanguageModel});
+            //dataList.Add(new { V = lansName[1], fixed_lans[1].LanguageModel });
+
+            //dataList.
+
+            //DataGridTextColumn[] Columns = new DataGridTextColumn[10]();
+            //var temp = fixed_lans.Join(fixed_lans, st =>
+            //    fixed_lans.IndexOf(st), et =>
+            //    fixed_lans.IndexOf(et), (st, et) =>
+            //    new { newst = st, newet = et }).ToList();
+            bool isNameAdded = false;
+            foreach (var data in fixed_lans)
+            {
+                if (!isNameAdded)
+                {
+                    DataGridTextColumn ColumnName = new DataGridTextColumn();
+                    ColumnName.Header = "Name";
+                    ColumnName.Binding = new Binding("Name");
+                    dg.Columns.Add(ColumnName);
+                    isNameAdded = true;
+                }
+                DataGridTextColumn Column = new DataGridTextColumn();
+                Column.Header = data.lanLabel;
+                Column.Binding = new Binding(data.lanLabel);
+                dg.Columns.Add(Column);
+                //xlvm.Add();
+            }
+
+            //dg.ItemsSource = fixed_lans;
+            dg.ItemsSource = expando;
+
+            //Load(lans);
         }
     }
+
 }
--- XmlSync/XmlLanguageModel.cs
+++ XmlSync/XmlLanguageModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace XmlSync
@@ -19,6 +20,13 @@ namespace XmlSync
         public int SecondID { get; set; }
         public string SecondText { get; set; }
     }
+    public class XmlMultiLanguageViewModel
+    {
+        public int ID { get; set; }
+        public int[] IDNames { get; set; }
+        public string[] Names { get; set; }
+        public string[] Texts { get; set; }
+    }
     public class Files
     {
         public int fileID { get; set; }
@@ -34,7 +42,7 @@ namespace XmlSync
         public int lanID { get; set; }
         public string lanLabel { get; set; }
         //public List<XmlLanguageModel> LanguageModel { get; set; }
-        private List<XmlLanguageModel> LanguageModel;
+        public List<XmlLanguageModel> LanguageModel;
         private bool isList { get; set; }
         private void CreateLanguageModel()
         {
@@ -48,6 +56,14 @@ namespace XmlSync
         {
             CreateLanguageModel();
             LanguageModel.Add(xmlLanguageModel);
+        }
+        public void SortModel()
+        {
+            LanguageModel = LanguageModel.OrderBy(o => o.Name).ToList();
+        }
+        public int LengthModel()
+        {
+            return LanguageModel.Count;
         }
         public List<XmlLanguageModel> GetModel()
         {
